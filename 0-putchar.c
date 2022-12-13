2a63bec85454699a390712f8a3500dd6418fe7d6
Wongani Kaunda <wonganikaunda2@gmail.com>
2022-12-13 06:49:06 -0800

    ahsjdhsad

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include "main.h"
+
+/**
+*main -printer putchar
+*return: 0 always
+*/
+int main(void)
+{
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
